b18f2a132b837181fdd7d2c3cc067e0932f52393
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-12-01 17:10:26 -0500

    Style zoom component

--- src/app/maps/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
-
+export class ZoomRangePageComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    
+    const map = new Map({
+      container: "map", // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
