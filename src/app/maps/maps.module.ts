e1dd8c723dc1c24d70bb9ed382160878432999cf
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-12-01 15:31:53 -0500

    Charge mapbox on maps module

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MapsRoutingModule } from './maps-routing.module';
-import { MiniMapComponent } from './maps/components/mini-map/mini-map.component';
-import { SidemenuComponent } from './maps/components/sidemenu/sidemenu.component';
-import { MapsLayoutComponent } from './maps/layout/maps-layout/maps-layout.component';
+import * as mapboxgl from 'mapbox-gl'; 
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoianVhbnJlc3RyZXBvd2ViZGV2IiwiYSI6ImNscG13ajJ1NjAwazEya29peTdreDZ0dmgifQ._2SBgMFVFjybgeeItJ-_SA';
+
 import { FullScreenPageComponent } from './maps/pages/full-screen-page/full-screen-page.component';
+import { MapsLayoutComponent } from './maps/layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { MarkersPageComponent } from './maps/pages/markers-page/markers-page.component';
+import { MiniMapComponent } from './maps/components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './maps/pages/properties-page/properties-page.component';
+import { SidemenuComponent } from './maps/components/sidemenu/sidemenu.component';
 import { ZoomRangePageComponent } from './maps/pages/zoom-range-page/zoom-range-page.component';
 
 
--- src/app/maps/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl'; 
-
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoianVhbnJlc3RyZXBvd2ViZGV2IiwiYSI6ImNscG13ajJ1NjAwazEya29peTdreDZ0dmgifQ._2SBgMFVFjybgeeItJ-_SA';
-
-
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-full-screen-page',
@@ -12,11 +8,10 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
-  
-
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+    
+    const map = new Map({
+      container: "map", // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
